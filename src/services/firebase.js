109330f2de81b7fff2f1ec77eb32d3c928aee268
Adam Alderson <alderson.ac@gmail.com>
2026-03-05 11:06:07 -0500

    chore: add real Firebase project config (event-registration-b7840)

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,15 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-// TODO: Replace with actual Firebase config from console
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "event-registration-system.firebaseapp.com",
-    projectId: "event-registration-system",
-    storageBucket: "event-registration-system.firebasestorage.app",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID",
-    measurementId: "YOUR_MEASUREMENT_ID"
+    apiKey: "AIzaSyCRD3xJf8Z5QVAfXpY6RFRYhg07YAkj16s",
+    authDomain: "event-registration-b7840.firebaseapp.com",
+    projectId: "event-registration-b7840",
+    storageBucket: "event-registration-b7840.firebasestorage.app",
+    messagingSenderId: "572635504019",
+    appId: "1:572635504019:web:7dcb6e42a7aee573e01c66",
+    measurementId: "G-YQ8YLTPH4M"
 };
 
 const app = initializeApp(firebaseConfig);
